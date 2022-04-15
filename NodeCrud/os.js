2d6d14072ff8b32154c5c28395c1edefd553e3c5
alamjamal <alamjamal88@gmail.com>
2022-04-15 16:27:44 +0530

    os change

--- NodeCrud/os.js
+++ NodeCrud/os.js
@@ -1,14 +1,13 @@
 const os=require('os');
 console.log(os.freemem());
 console.log(os.totalmem()/1024);
+console.log(os.arch());
+
 
 const path=require('path');
 console.log(path.extname('C:/Users/Alam Jamal/MyNode/os.js'));
-console.log(os.arch());
 console.log(path.basename('C:/Users/Alam Jamal/MyNode/os.js'));
-
 console.log(path.parse('C:/Users/Alam Jamal/MyNode/os.js'));
-
 const mypath=path.parse('C:/Users/Alam Jamal/MyNode/os.js');
 console.log(mypath.base);
 console.log("alam");
